9d51a97fceb89db41010158f0efc52639cf2bd20
Hanul Lee <gksdnf719@naver.com>
2018-06-15 08:33:34 +0000

    day7

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,11 @@ Rails.application.routes.draw do
   get'/lotto' => 'lotto#index'
   get '/lotto/new' => 'lotto#new'
   
+  get '/ask' => 'ask#index'
+  get '/ask/new' => 'ask#new'
+  post '/ask/create' => 'ask#create'
+  get '/ask/:id/delete' => 'ask#delete'
+  get '/ask/:id/edit' => 'ask#edit'
+  post '/ask/:id/update' => 'ask#update'
+  get '/ask/:id' => 'ask#show'
 end
